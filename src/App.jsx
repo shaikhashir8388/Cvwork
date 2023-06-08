57b5e54a0adee657738bf2960521b1631b0a4ab9
shaikhashir8388 <115325745+shaikhashir8388@users.noreply.github.com>
2023-06-09 00:54:08 +0500

    seconf commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div className='cvwork'>
+
+      <h1>hey</h1>
+      <p>I am a front-end developer who loves building user friendly websites and enjoys writing documentation and loves helping people learn how to code. I am Familiar with HTML5, CSS, JavaScript. I also use same Libraries like Reac¬t JS, JQuery. Most recently, I've done some projects and web responsive designs. Some Extra Skills like: MS Word, MS Excel, MS Powerpoint,
+I am currently interested in learning more about accessibility and delving deeper into CSS, React JS.
+</p>
+<h1>ACADEMICS</h1>
+<p>Bachelor of Science : Computer Science
+Federal University : Block 9, Gulshan E Iqbal, Karachi 2020 - 01 — Current
+
+</p>
+<h1>PERSONAL ATTRIBUTES</h1>
+<p>Father's Name	: Zaheer Ahmed Marital Status	: Single
+Known Languages : English • Urdu
+
+CNIC No.	: 42201-3980178-1
+
+Date of Birth	: Feb 10th, 2002 Personality	: Active & Pleasant
+</p>
+<h1>COURSES</h1>
+<p>Responsive Web Design HTML5 & CSS3 
+Pirple.com
+
+Smart Web Designing Skill Development   Council, Karachi
+</p>
+<h1>REFERENCE</h1>
+<p>Adequate References May Be Furnished If Desired! - ""</p>
+      
+      
+      
+      
+      </div>    
+    </>
+  );
+}
+
+export default App;
